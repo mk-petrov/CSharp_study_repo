0740180eb08b3309e0f383757bc16e79e8bb28ef
Petko Petrov <petko.petrov89@gmail.com>
2017-07-28 13:21:13 +0300

    added new class

---
+++ SimpleObjectsExercises/OptimizedBankingSystem/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizedBankingSystem
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+
+        public string Bank { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public static BankAccount Parse (string inputLine)
+        {
+            var input = inputLine
+                .Split(new char[] { '|', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            return new BankAccount
+            {
+                Bank = input[0],
+                Name = input[1],
+                Balance = decimal.Parse(input[2])
+            };           
+            
+        }
+    }
+}
---
+++ SimpleObjectsExercises/OptimizedBankingSystem/BankingSystem.cs
@@ -0,0 +1,35 @@
+﻿
+namespace OptimizedBankingSystem
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BankingSystem
+    {
+        public static void Main()
+        {
+            var bankAccountData = new List<BankAccount>();
+            var line = Console.ReadLine();
+
+            while (!line.Equals("end"))
+            {
+                var currentAccount = BankAccount.Parse(line);
+                               
+                
+                bankAccountData.Add(currentAccount);
+
+                line = Console.ReadLine();
+            }
+
+            var sortedByAccount = bankAccountData.OrderByDescending(b => b.Balance)
+                    .ThenBy(n => n.Bank.Length).ToArray();
+
+            foreach (var account in sortedByAccount)
+            {
+                
+                Console.WriteLine($"{account.Name} -> {account.Balance} ({account.Bank})");
+            }
+        }
+    }
+}
