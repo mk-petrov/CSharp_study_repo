8ee4ecc2544f1c20c5ff568815c0310ace59f69e
Petko Petrov <petko.petrov89@gmail.com>
2017-07-28 12:15:46 +0300

    added ExerciseProblem

---
+++ SimpleObjectsExercises/SimpleObjectsExercises/Exercise.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleObjectsExercises
+{
+    public class Exercise
+    {
+        public string Topic { get; set; }
+
+        public string CourseName { get; set; }
+
+        public string JudgeContestLink { get; set; }
+
+        public List<string> problems = new List<string>();
+
+        public List<string> cases { get; set; }
+
+        public List<string> Problems
+        {
+            get
+            {
+                return problems;
+            }
+        }
+
+        public void AddProblem (string line)
+        {
+            Problems.Add(line);
+        }
+
+        public static Exercise Parse(string line)
+        {
+            var input = line
+            .Split(new char[] { ' ', '-', '>', ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+
+            var inputRest = input.Skip(3).ToList();
+
+            return new Exercise
+            {
+                Topic = input[0],
+                CourseName = input[1],
+                JudgeContestLink = input[2],
+                //problems = inputRest
+                cases = inputRest
+            };
+        }
+    }
+}
---
+++ SimpleObjectsExercises/SimpleObjectsExercises/Exercises.cs
@@ -0,0 +1,49 @@
+﻿
+namespace SimpleObjectsExercises
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Exercises
+    {
+        public static void Main()
+        {
+            var line = Console.ReadLine();
+            var exercisesData = new List<Exercise>();
+
+            while (line != "go go go")
+            {               
+                var currentExercise = Exercise.Parse(line);
+
+                exercisesData.Add(currentExercise);
+
+                line = Console.ReadLine();
+            }
+                        
+            Console.WriteLine();
+
+            foreach (var item in exercisesData)
+            {
+                var counter = 1;
+                Console.WriteLine($"Exercises: {item.Topic}");
+                Console.WriteLine($"Problems for exercises and homework" +
+                    $" for the \"{item.CourseName}\" course @ SoftUni.");
+                Console.WriteLine($"Check your solutions here: {item.JudgeContestLink}.");
+
+                for (int i = 0; i < item.cases.Count; i++)
+                {
+                    Console.WriteLine($"{counter}. {item.cases[i]}");
+                    counter++;
+                }
+
+                //for (int i = 0; i < item.Problems.Count; i++)
+                //{
+                //    Console.WriteLine($"{counter}. {item.Problems[i]}");
+                //    counter++;
+                //}
+            }
+            
+        }
+    }
+}
