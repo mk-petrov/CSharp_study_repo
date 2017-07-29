7eeb231f62f7fb0ca6bc310d1fa07cb05de9913c
Petko Petrov <petko.petrov89@gmail.com>
2017-07-29 12:10:56 +0300

    added new project

---
+++ SimpleObjectsExercises/Websites/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Websites
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var websiteData = new List<Website>();
+
+            var line = Console.ReadLine();
+
+            while ( !line.Equals("end"))
+            {
+                var currentWebsite = Website.Parse(line);
+                                
+                websiteData.Add(currentWebsite);
+
+                line = Console.ReadLine();
+            }
+
+            foreach (var website in websiteData)
+            {
+                Console.Write($"https://www.{website.Host}.{website.Domain}");
+
+                if (website.Queries.Count > 0)
+                {
+                    Console.Write($"/query?=");
+
+                    for (int i = 0; i < website.Queries.Count; i++)
+                    {
+                        Console.Write($"[{website.Queries[i]}]");
+
+                        if (i == 0 || i < website.Queries.Count - 1)
+                        {
+                            Console.Write("&");
+                        }                      
+                    }
+                    Console.WriteLine();
+                }                                              
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SimpleObjectsExercises/Websites/Website.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Websites
+{
+    public class Website
+    {
+        public string Host { get; set; }
+
+        public string Domain { get; set; }
+
+        public List<string> Queries { get; set; }
+
+        public static Website Parse (string line)
+        {
+            var input = line
+                .Split(new char[] { ' ', '|', ',' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+
+            return new Website
+            {
+                Host = input[0],
+                Domain = input[1],
+                Queries = input.Skip(2).ToList()
+            };
+        }
+    }
+}
