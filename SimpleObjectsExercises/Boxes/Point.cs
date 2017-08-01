7c08fea61e92babc7c4ce35dff0b8352a6cf658b
Petko Petrov <petko.petrov89@gmail.com>
2017-08-01 11:24:25 +0300

    added project Rectangular

---
+++ SimpleObjectsExercises/Boxes/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Boxes
+{
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public static double CalculateDistance(Point point1, Point point2)
+        {
+            var distanceX = Math.Pow((point2.X - point1.X), 2);
+            var distanceY = Math.Pow((point2.Y - point1.Y), 2);
+
+            var distance = Math.Sqrt(distanceX + distanceY);
+
+            return distance;
+        }
+    }
+}
---
+++ SimpleObjectsExercises/Boxes/Program.cs
@@ -0,0 +1,79 @@
+﻿
+namespace Boxes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var rectangulares = new List<Rectangular>();
+            var line = Console.ReadLine();
+
+            while ( !line.Equals("end"))
+            {
+                var rectangular = ParseInputToRectangular(line);
+
+                rectangulares.Add(rectangular);
+
+                line = Console.ReadLine();
+            }
+
+            foreach (var rectangular in rectangulares)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Box: {rectangular.Width}, {rectangular.Height}");
+                Console.WriteLine($"Perimeter: " +
+                    $"{Rectangular.CalculatePerimeter(rectangular.Width, rectangular.Height)}");
+                Console.WriteLine($"Area:" +
+                    $" {Rectangular.CalculateArea(rectangular.Width, rectangular.Height)}");
+            }
+
+        }
+
+        public static Rectangular ParseInputToRectangular(string line)
+        {
+            var input = line
+                .Split(new char[] { ' ', '|', ':' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            var upperLeft = new Point
+            {
+                X = input[0],
+                Y = input[1]
+            };
+
+            var upperRight = new Point
+            {
+                X = input[2],
+                Y = input[3]
+            };
+
+            var bottomLeft = new Point
+            {
+                X = input[4],
+                Y = input[5]
+            };
+
+            var bottomRight = new Point
+            {
+                X = input[6],
+                Y = input[7]
+            };
+
+            var rectangular = new Rectangular
+            {
+                UpperLeft = upperLeft,
+                UpperRight = upperRight,
+                BottomLeft = bottomLeft,
+                BottomRight = bottomRight
+            };
+
+            return rectangular;
+        }
+    }
+}
