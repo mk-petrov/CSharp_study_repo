227bda93e5c98b5fe91144074bca02b1d7c29f8b
Petko Petrov <petko.petrov89@gmail.com>
2017-08-04 13:41:03 +0300

    printFile method

---
+++ FilesAndDirectories/FilesAndDirectories/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace FilesAndDirectories
+{
+    using System;
+    using System.IO;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            //PrintDirectoriesAndFilesInIt();
+
+
+        }
+
+        private static void PrintDirectoriesAndFilesInIt()
+        {
+            string[] subDirectoryNames = Directory.GetDirectories("Test");
+
+            foreach (var subDirectory in subDirectoryNames)
+            {
+                Console.WriteLine("{0}", subDirectory.Replace("Test\\", ""));
+
+                string[] fileNames = Directory.GetFiles(subDirectory);
+
+                foreach (var file in fileNames)
+                {
+                    Console.WriteLine("*** {0}",
+                        file.Split('\\')[file.Split('\\').Length - 1]);
+                }
+            }
+        }
+    }
+}
