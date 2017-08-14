f6ff8f0a9f4d2171f16ebbb8b9b7a53fd9dc6837
Petko Petrov <petko.petrov89@gmail.com>
2017-08-14 14:58:06 +0300

    new project started

---
+++ FilesAndDirectories/Products/Product.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Products
+{
+    public class Product
+    {
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Type { get; set; }
+
+        public string Name { get; set; }
+                
+        public static Product Parse(string[] input)
+        {
+            
+            return new Product
+            {
+                Name = input[0],
+                Type = input[1],
+                Price = decimal.Parse(input[2]),
+                Quantity = int.Parse(input[3])
+            };
+        }
+    }
+}
---
+++ FilesAndDirectories/Products/ProductsData.cs
@@ -0,0 +1,92 @@
+﻿
+namespace Products
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class ProductsData
+    {
+        private static Dictionary<string, Product> database =
+            new Dictionary<string, Product>();
+
+        private static string dbPath = "ProductsDatabase.txt";
+
+        public static void Main()
+        {
+
+            //“Food”, “Electronics”, “Domestics”.
+
+            //LOAD DATA FROM DATABASE
+            string[] products = File.ReadAllLines(dbPath);
+
+            foreach (var product in products)
+            {
+                var productParts = product.Split(' ', '|');
+                var name = productParts[0];
+                var type = productParts[1];
+                
+                database[productParts[0]]
+            }
+
+            var line = Console.ReadLine();
+
+            while (!line.Equals("exit"))
+            {
+                var input = line.Split(' ');
+
+                if (input.Length > 1)
+                {
+                    var newProduct = Product.Parse(input);
+
+                    //if ( database.ContainsKey(newProduct.Name))
+                    //{
+                    //    if (database[newProduct.Name].Type == input[1])
+                    //    {
+                    //        database[newProduct.Name].Price = decimal.Parse(input[2]);
+                    //        database[newProduct.Name].Quantity = int.Parse(input[3]);
+                    //    }
+                         
+                    //}
+
+                    database[newProduct.Name] = newProduct;
+                }
+                else
+                {
+                    //sales; analyze; stock 
+                    switch (input[0])
+                    {
+                        case "sales":
+
+                            break;
+                        case "stock":
+                            StockDatabase();
+                            break;
+                        case "analyze":
+
+                            break;
+                    }
+                }
+
+                line = Console.ReadLine();
+            }
+
+
+        }
+
+        private static void StockDatabase()
+        {
+            
+            if ( !File.Exists(dbPath))
+            {
+                File.Create(dbPath).Close();
+            }
+
+            foreach (var data in database)
+            {
+                File.AppendAllLines(dbPath,new[] { $"{data.Key} | {data.Value.Type} | {data.Value.Price} | {data.Value.Quantity}" });
+            }
+        }
+    }
+}
