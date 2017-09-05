5f1cbf0477235c73efccf47b9732c095faa3e3e6
Petko Petrov <petko.petrov89@gmail.com>
2017-09-05 10:39:41 +0300

    Json parse task finished

--- StringsAndText/JSON_stringify/JSON.cs
+++ StringsAndText/JSON_stringify/JSON.cs
@@ -11,23 +11,61 @@ namespace JSON_stringify
         public static void Main()
         {
             var studentsBase = new Dictionary<string, Student>();
-
+            var studentsList = new List<string>();
             var line = Console.ReadLine();
 
             //line = ToJSON(studentsBase, line);    //ENABLE FROM STRING TO JSON METHOD
 
-            var info = line.Trim('{', '}').Split(':', ',');
-            //.Trim('{', '}', '"', ':', '[', ']')
+            var infoFromJSON = line.Trim('[', ']').Split(new[] { '}' },
+                StringSplitOptions.RemoveEmptyEntries).ToArray();
 
-            var name = info[1].Trim('"');     // name|"Ivan"|age|28|grades|[2| 2| 3]
-            var age = int.Parse(info[3]);
-            var grades = info.Skip(5).ToArray();
-            //grades = grades.Trim('[', ']');
+            foreach (var info in infoFromJSON)
+            {                
+                studentsList.Add(info.Trim(',', '{'));
+            }
 
-            Console.WriteLine(string.Join("|", info));
-            Console.WriteLine(name + " is " + age);
-            Console.WriteLine("grades: "+ string.Join("", grades));
+            foreach (var studentInfo in studentsList)
+            {
+                var infoParams = studentInfo.Split(new[] { ':', ',' },
+                    StringSplitOptions.RemoveEmptyEntries);
 
+                var name = infoParams[1].Trim('"');
+                var age = int.Parse(infoParams[3]);
+                var gradesInfo = infoParams.Skip(5).ToArray();
+                var grades = new List<int>();
+
+                for (int i = 0; i < gradesInfo.Length; i++)
+                {
+                    int grade;
+                    bool isGrade = int.TryParse(gradesInfo[i].Trim('[', ']', ' '), out grade);
+
+                    if (isGrade)
+                    {
+                        grades.Add(grade);
+                    }                    
+                }
+
+                Student newStudent = new Student();
+                newStudent.Name = name;
+                newStudent.Age = age;
+                newStudent.Grades = grades;
+
+                studentsBase[newStudent.Name] = newStudent;
+                                                
+            }
+
+            foreach (var kvp in studentsBase)
+            {
+                if (kvp.Value.Grades.Count == 0)
+                {
+                    Console.WriteLine($"{kvp.Key} : {kvp.Value.Age} -> None");
+                }
+                else
+                {
+                    Console.WriteLine($"{kvp.Key} : {kvp.Value.Age} -> {string.Join(", ", kvp.Value.Grades)}");
+                }
+            }
+                        
         }
 
         private static string ToJSON(Dictionary<string, Student> studentsBase, string line)
