6c2f5e9429030c0c611b347a08ea5ddc5dcafc21
Petko Petrov <petko.petrov89@gmail.com>
2017-09-05 11:37:07 +0300

    Sentence Split task solved

---
+++ StringsAndText/SentenceSplit/SentenceSplit.cs
@@ -0,0 +1,19 @@
+﻿
+namespace SentenceSplit
+{
+    using System;
+    using System.Text;
+
+    public class SentenceSplit
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var delimiter = Console.ReadLine();
+
+            var partsOfInput = input.Split(new string[] { delimiter }, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine("[{0}]",string.Join(", ", partsOfInput));            
+        }
+    }
+}
