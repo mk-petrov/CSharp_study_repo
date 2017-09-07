f6d105bf7d9b28d5c2d0cc4d305814bf8cd21a1b
Petko Petrov <petko.petrov89@gmail.com>
2017-09-07 13:34:02 +0300

    pyramidic task solved

---
+++ StringsAndText/Pyramidic/Pyramidic.cs
@@ -0,0 +1,56 @@
+﻿namespace Pyramidic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Pyramidic
+    {
+        public static void Main()
+        {
+            List<string> pyramids = new List<string>();
+
+            int n = int.Parse(Console.ReadLine());
+            string[] lines = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                lines[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string currentLine = lines[i];
+
+                for (int j = 0; j < currentLine.Length; j++)
+                {
+                    char currentChar = currentLine[j];
+                    int layer = 1;
+                    string currentPyramid = "";
+
+                    for (int k = i; k < lines.Length; k++)
+                    {
+                        string currentLayer = new string(currentChar, layer);
+
+                        if (lines[k].Contains(currentLayer))
+                        {
+                            currentPyramid += currentLayer + "\r\n";
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                        layer += 2;
+                    }
+
+                    pyramids.Add(currentPyramid.Trim());
+                }
+            }
+
+            Console.WriteLine("+++ The biggest Pyramid +++");
+            Console.WriteLine(pyramids.OrderByDescending(x => x.Length).First());
+
+        }
+    }
+}
---
+++ StringsAndText/SerializeString/SerializeString.cs
@@ -0,0 +1,31 @@
+﻿
+namespace SerializeString
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class SerializeString
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<char, List<int>> letters = new Dictionary<char, List<int>>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!letters.ContainsKey(input[i]))
+                {
+                    letters[input[i]] = new List<int>();
+                }
+
+                letters[input[i]].Add(i);
+            }
+
+            foreach (var kvp in letters)
+            {
+                Console.WriteLine($"{kvp.Key}:{string.Join("/", kvp.Value)}");
+            }
+        }
+    }
+}
