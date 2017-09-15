59709b5c62f4ae3175c92489b0024edddc8287db
Petko Petrov <petko.petrov89@gmail.com>
2017-09-15 15:26:10 +0300

    <a> tag replace added

---
+++ RegEx/ReplaceAtag/ReplaceTag.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ReplaceATag
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class ReplaceTag
+    {
+        static void Main()
+        {                        
+            var pattern = @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+
+            var text = "<ul> <li> <a href=\"http://softuni.bg\">SoftUni</a> </ li > </ ul > ";
+
+            var regex = new Regex(pattern);
+
+            var replace = @"[URL href=$1]$2[/URL]";
+
+            var replaced = regex.Replace(text, replace);
+
+            Console.WriteLine(replaced);
+
+        }
+    }
+}
