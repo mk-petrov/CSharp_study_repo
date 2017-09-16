9996bf3738c8305c07089f388d8aef4ccf7c4587
Petko Petrov <petko.petrov89@gmail.com>
2017-09-16 13:03:49 +0300

    Cards task added

---
+++ RegEx/Cards/Cards.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Cards
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class Cards
+    {
+        public static void Main()
+        {
+            var validCards = "[2345678910JQKA][SHDC]";
+            var regex = new Regex(validCards);
+
+            var line = Console.ReadLine();
+
+            var cards = regex.Matches(line);
+
+            var cardsAsArray = new string[cards.Count];
+
+            for (int i = 0; i < cards.Count; i++)
+            {
+                var currentCard = cards[i].ToString();
+                cardsAsArray[i] = currentCard;
+            }
+
+            Console.WriteLine(string.Join(", ", cardsAsArray));
+        }
+    }
+}
