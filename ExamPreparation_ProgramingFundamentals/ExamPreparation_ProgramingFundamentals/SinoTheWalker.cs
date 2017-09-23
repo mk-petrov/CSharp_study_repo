628a450a9ea79cba85fd4a27d6924524266945f8
Petko Petrov <petko.petrov89@gmail.com>
2017-09-23 09:56:22 +0300

    added two projects

---
+++ ExamPreparation_ProgramingFundamentals/ExamPreparation_ProgramingFundamentals/SinoTheWalker.cs
@@ -0,0 +1,43 @@
+﻿
+namespace ExamPreparation_ProgramingFundamentals
+{
+    using System;
+    using System.Globalization;
+
+    public class SinoTheWalker
+    {
+        public static void Main()
+        {
+            var timeFormat = @"hh\:mm\:ss";
+            var leavingTime = TimeSpan.ParseExact(Console.ReadLine(), timeFormat, CultureInfo.InvariantCulture);
+
+            var stepsNeeded = decimal.Parse(Console.ReadLine());
+            var secondsPerStep = decimal.Parse(Console.ReadLine());
+
+            var secondsInADay = 60 * 60 * 24;
+
+            // to protect it if it's > int.MaxValue
+            var totalSecondsNeeded = (int)(stepsNeeded * secondsPerStep % secondsInADay);
+
+            var arrivalTime = leavingTime.Add(new TimeSpan(0, 0, totalSecondsNeeded));
+
+            //ToString in needed, because will print and days if is later than 23:59:59
+            Console.WriteLine("Time Arrival: {0}", arrivalTime.ToString(timeFormat));
+            
+
+            //SECOND SOLUTION
+
+            //var timeFormatTwo = "HH:mm:ss";
+            //var leavingTimeTwo = DateTime
+            //    .ParseExact(Console.ReadLine(), timeFormatTwo, CultureInfo.InvariantCulture);
+
+            //var stepsNeededTwo = decimal.Parse(Console.ReadLine());
+            //var secPerStep = decimal.Parse(Console.ReadLine());
+
+            //var neededSeconds = (double)(stepsNeededTwo * secPerStep % secondsInADay);
+
+            //var arrivalTimeTwo = leavingTimeTwo.AddSeconds(neededSeconds);
+
+        }
+    }
+}
---
+++ ExamPreparation_ProgramingFundamentals/SoftUni_Karaoke/SoftUni_Karaoke.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUni_Karaoke
+{
+    public class SoftUni_Karaoke
+    {
+        public static void Main()
+        {
+            var participants = Console.ReadLine()
+                .Split(',')
+                .Select(a => a.Trim())
+                .ToArray();
+
+            var songs = Console.ReadLine()
+                .Split(',')
+                .Select(a => a.Trim())
+                .ToArray();
+
+            var participantAwards = new Dictionary<string, HashSet<string>>();  // or List.Distinct ;)
+
+            var line = Console.ReadLine();
+            while (line != "dawn")
+            {
+                var tokens = line.Split(',')
+                .Select(a => a.Trim())
+                .ToArray();
+
+                var participant = tokens[0];
+                var song = tokens[1];
+                var award = tokens[2];
+
+                if (songs.Contains(song) && participants.Contains(participant))
+                {
+                    if (!participantAwards.ContainsKey(participant))
+                    {
+                        participantAwards[participant] = new HashSet<string>();
+                    }
+
+                    participantAwards[participant].Add(award);
+                }
+
+                line = Console.ReadLine();
+            }
+
+            if (!participantAwards.Any()) // !participantAwarda.Count == 0
+            {
+                Console.WriteLine("No awards");
+                return;
+            }
+
+            var sortedParticipantAwards = participantAwards
+                .OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key)
+                .ToDictionary(a => a.Key, a => a.Value);
+
+            foreach (var kvp in sortedParticipantAwards)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Count} awards");
+
+                foreach (var award in kvp.Value.OrderBy(a => a))   // or use SortedSet to avoid .OrderBy()
+                {
+                    Console.WriteLine("--{0}", award);
+                }
+            }
+        }
+    }
+}
