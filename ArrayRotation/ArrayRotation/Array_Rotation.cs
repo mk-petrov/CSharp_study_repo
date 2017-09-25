0e1f575e9dc6c88eabb2258675f53d56ca37f2af
Petko Petrov <petko.petrov89@gmail.com>
2017-09-25 11:32:44 +0300

    simple rotation

---
+++ ArrayRotation/ArrayRotation/Array_Rotation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayRotation
+{
+    public class Array_Rotation
+    {
+        public static void Main()
+        {
+            var arrayLenght = int.Parse(Console.ReadLine());
+            decimal numberOfRotation = decimal.Parse(Console.ReadLine()); // decimal if input is like: 14578914521
+            var arrayOfNumbers = new int[arrayLenght];
+            var rotatedArray = new int[arrayLenght];
+
+            //fill the array with numbers
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                arrayOfNumbers[i] = i + 1;
+            }
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                var rotateTo = (i + (int)numberOfRotation) % arrayOfNumbers.Length;
+                var currentNum = arrayOfNumbers[i];
+                rotatedArray[rotateTo] = currentNum;
+            }
+
+            Console.WriteLine("Original array:");
+            Console.WriteLine(string.Join("|", arrayOfNumbers));
+            Console.WriteLine("Rotated array:");
+            Console.WriteLine(string.Join("|", rotatedArray));
+        }
+    }
+}
