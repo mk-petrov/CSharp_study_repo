8f1ddcd98d601395cfc5508939ee17fd05a6858e
Petko Petrov <petko.petrov89@gmail.com>
2017-09-26 14:13:08 +0300

    Nether_Realms.cs and Demon.cs finished

---
+++ ExamPreparation_ProgramingFundamentals/Nether_Realms/Demon.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Nether_Realms
+{
+    public class Demon
+    {
+        public string Name { get; set; }
+
+        public double Damage { get; set; }
+
+        public int Health { get; set; }
+    }
+}
--- ExamPreparation_ProgramingFundamentals/Nether_Realms/Nether_Realms.cs
+++ ExamPreparation_ProgramingFundamentals/Nether_Realms/Nether_Realms.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Nether_Realms
@@ -13,6 +13,8 @@ namespace Nether_Realms
             var demons = Console.ReadLine()
                  .Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
                  .ToArray();
+
+            var result = new SortedDictionary<string, Demon>();
 
             foreach (var demon in demons)
             {
@@ -31,8 +33,50 @@ namespace Nether_Realms
                 {
                     health += healthSymbol;
                 }
-            }
 
+                // to match: -127.27 or 5.4 or 7
+                // zero or one: - ; one or more: nums; zero or one: . ; zero or more nums;
+                var regex = new Regex(@"-?\d+\.?\d*");
+
+                var matches = regex.Matches(demon);
+
+                var damage = 0.0;
+
+                foreach (Match match in matches)
+                {
+                    var currentNumber = double.Parse(match.Value);
+
+                    damage += currentNumber;
+                }
+
+                var modifiers = demon.Where(s => s == '/' || s == '*');
+
+                foreach (var modifier in modifiers)
+                {
+                    if (modifier == '*')
+                    {
+                        damage *= 2;
+                    }
+                    else if (modifier == '/')
+                    {
+                        damage /= 2;
+                    }
+                }
+
+                result.Add(demon, new Demon
+                {
+                    Name = demon,
+                    Health = health,
+                    Damage = damage
+                });
+
+            }
+            foreach (var demon in result)
+            {
+                Console.WriteLine($"{demon.Key} - {demon.Value.Health} health, " +
+                    $"{demon.Value.Damage:F2} damage");
+            }
+            Console.WriteLine();
         }
     }
 }
