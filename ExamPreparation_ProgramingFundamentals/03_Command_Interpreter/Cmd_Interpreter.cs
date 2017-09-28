1dc148ffeb808aa85a2fbbc1835629bf5216a5d5
Petko Petrov <petko.petrov89@gmail.com>
2017-09-28 16:31:58 +0300

    tasks from 03 added

---
+++ ExamPreparation_ProgramingFundamentals/03_Command_Interpreter/Cmd_Interpreter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Command_Interpreter
+{
+    public class Cmd_Interpreter
+    {
+        public static void Main()
+        {
+            List<string> array = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "end")
+            {
+                string[] inputParams = inputLine.Split();
+
+                string command = inputParams[0];
+
+                switch (command)
+                {
+                    case "reverse":
+                        int reverseStart = int.Parse(inputParams[2]);
+                        int reverseCount = int.Parse(inputParams[4]);
+
+                        Reverse(array, reverseStart, reverseCount);
+
+
+                        break;
+                    case "sort":
+                        int sortStart = int.Parse(inputParams[2]);
+                        int sortCount = int.Parse(inputParams[4]);
+
+                        Sort(array, sortStart, sortCount);
+
+                        break;
+                    case "rollLeft":
+                        //
+                        break;
+                    case "rollRight":
+                        //
+                        break;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+        }
+
+        private static void Sort(List<string> array, int sortStart, int sortCount)
+        {
+            array.Sort(sortStart, sortCount, null); 
+        }
+
+        private static void Reverse(List<string> array, int reverseStart, int reverseCount)
+        {
+            array.Reverse(reverseStart, reverseCount);
+        }
+    }
+}
---
+++ ExamPreparation_ProgramingFundamentals/03_SoftUni_Coffee_Orders/Coffee_Orders.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _03_SoftUni_Coffee_Orders
+{
+    public class Coffee_Orders
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal totalPrice = 0M;    // D -double, F-float, M-decimal
+
+            for (int i = 0; i < n; i++)
+            {
+                decimal pricePerCapsule = decimal.Parse(Console.ReadLine());
+                DateTime orderDate = DateTime
+                    .ParseExact(Console.ReadLine(), "d/M/yyyy", CultureInfo.InvariantCulture);
+                int capsuleCount = int.Parse(Console.ReadLine());
+
+                decimal currentPrice = ((DateTime.
+                    DaysInMonth(orderDate.Year, orderDate.Month) * capsuleCount) * (long)pricePerCapsule);
+
+                Console.WriteLine("The price for the coffee is: ${0:F2}", currentPrice);
+
+                totalPrice += currentPrice;
+            }
+
+            Console.WriteLine($"Total: ${totalPrice:F2}");
+        }
+    }
+}
