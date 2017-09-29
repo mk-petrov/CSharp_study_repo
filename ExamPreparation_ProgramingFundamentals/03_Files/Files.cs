14be85d2fff93456828b092d65855feb3d06dd6d
Petko Petrov <petko.petrov89@gmail.com>
2017-09-29 13:30:57 +0300

    :)

---
+++ ExamPreparation_ProgramingFundamentals/03_Files/Files.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _03_Files
+{
+    public class Files
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ ExamPreparation_ProgramingFundamentals/03_Rage_Quit/Rage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _03_Rage_Quit
+{
+    public class Rage
+    {
+        public static void Main()
+        {
+            // INPUT LIKE: 
+            //
+
+            var pattern = @"(\D+)(\d+)";
+
+            var regex = new Regex(pattern);
+
+            string inputLine = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(inputLine);
+
+            StringBuilder result = new StringBuilder();
+            
+
+            foreach (Match match in matches)
+            {
+                string partition = match.Groups[1].Value;
+                int times = int.Parse(match.Groups[2].Value);
+
+                result.Append(Repeat(partition, times).ToUpper());
+            }
+
+            int uniqueSymbols = result.ToString().Distinct().Count();
+
+            Console.WriteLine($"Unique symbols used: {uniqueSymbols}");
+            Console.WriteLine(result);
+        }
+
+        private static string Repeat(string partition, int times)
+        {
+            StringBuilder final = new StringBuilder();
+
+            for (int i = 0; i < times; i++)
+            {
+                final.Append(partition);
+            }
+
+            return final.ToString();
+        }
+    }
+}
