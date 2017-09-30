b3edf519366752c9218f073ed1408772049cbeea
Petko Petrov <petko.petrov89@gmail.com>
2017-09-30 12:29:09 +0300

    files task started

--- ExamPreparation_ProgramingFundamentals/03_Files/Files.cs
+++ ExamPreparation_ProgramingFundamentals/03_Files/Files.cs
@@ -9,7 +9,25 @@ namespace _03_Files
     {
         public static void Main()
         {
+            int n = int.Parse(Console.ReadLine());
 
+            var filesByRoot = new Dictionary<string, Dictionary<string, long>>(); //root -> name:size
+            var filesByExtension = new Dictionary<string, string>();  //extension:name
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputParams = Console.ReadLine().Split('\\');
+                string root = inputParams[0];
+                string[] fileParams = inputParams[inputParams.Length - 1]
+                    .Split(new[] { '.', ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string fileName = fileParams[0];
+                string fileExtension = fileParams[1];
+                long fileSize = long.Parse(fileParams[2]);
+
+            }
+
+            //QUERY 
         }
     }
 }
