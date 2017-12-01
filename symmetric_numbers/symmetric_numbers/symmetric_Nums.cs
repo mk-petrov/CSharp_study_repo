6282fd5cd4c8ede7f421a0dd66eccb54c8c29f6a
Petko Petrov <petko.petrov89@gmail.com>
2017-12-01 16:57:05 +0200

    symmetric_numbers in C# solved

---
+++ symmetric_numbers/symmetric_numbers/symmetric_Nums.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace symmetric_numbers
+{
+    public class symmetric_Nums
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var symmetricNumbers = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                var currentNumberAsString = i.ToString();
+                var currentNumLength = currentNumberAsString.Length;
+                if (currentNumLength > 1)
+                {
+                    for (int j = 0; j < currentNumLength / 2 ; j++)
+                    {
+                        if (currentNumberAsString[j] == currentNumberAsString[currentNumLength - j - 1])
+                        {
+                            symmetricNumbers.Add(i);
+                        }
+                    }
+                }
+                else
+                {
+                    symmetricNumbers.Add(i);
+                }
+
+            }
+            Console.WriteLine(String.Join(", ", symmetricNumbers));
+        }
+    }
+}
